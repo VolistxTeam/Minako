28f5803e367e2262484c64e33964afc0c1e3dffe
Cryental <contact@cryental.dev>
2021-06-09 18:25:47 +0600

    Routing updated

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,7 @@ $router->group(['middleware' => ['cacheResponse']], function () use ($router) {
     $router->group(['prefix' => 'ohys'], function () use ($router) {
         $router->get('/', 'Services\OhysController@GetRecentTorrents');
         $router->get('/{id}', 'Services\OhysController@GetTorrent');
+        $router->get('/download/{id}', 'Services\OhysController@DownloadTorrent');
         $router->get('/search/{name}', 'Services\OhysController@Search');
     });
 });
