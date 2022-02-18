94b44a68a48ddbac3d84fce6dc549fabc68cb502
Cryental <contact@cryental.com>
2022-02-18 13:55:38 +0600

    Updated Character Image Link

--- app/Http/Controllers/Services/CharacterController.php
+++ app/Http/Controllers/Services/CharacterController.php
@@ -75,7 +75,7 @@ class CharacterController extends Controller
                 'width'  => $itemQuery->image_width,
                 'height' => $itemQuery->image_height,
                 'format' => 'jpg',
-                'link'   => '',
+                'link'   => env('APP_URL', 'http://localhost').'/character/' . $itemQuery->uniqueID . '/image',
             ],
             'attributes' => $itemQuery->attributes,
             'mappings'   => $filteredMappingData,
