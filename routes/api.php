91e9a836b27a31d5c774fe21d4e095aeb48a1f78
Cryental <contact@cryental.com>
2022-06-06 11:31:49 -0600

    Added RSS

--- routes/api.php
+++ routes/api.php
@@ -50,5 +50,5 @@ $router->group(['prefix' => 'ohys'], function () use ($router) {
     $router->get('/{id}', 'Services\OhysController@GetTorrent');
     $router->get('/{id}/download', 'Services\OhysController@DownloadTorrent');
     $router->get('/search/{name}', 'Services\OhysController@Search');
-    $router->get('/rss', 'Services\OhysController@GetRSS');
+    $router->get('/client/rss', 'Services\OhysController@GetRSS');
 });
