021b5b8407de52f8a02c9fadd30f1e256aaf4419
I'm Not Cry <44664655+Cryental@users.noreply.github.com>
2023-04-12 23:07:39 +0600

    Update trustedproxy.php

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -6,7 +6,7 @@ return [
     'proxies' => [
         // Local Proxy
         '127.0.0.1',
-        '80.78.25.105',
+        '185.196.222.105',
     ],
 
     'headers' => Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO,
