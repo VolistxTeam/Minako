a9892cbb3f475f9dd567639701f4aa03d7012264
Cryental <contact@cryental.dev>
2024-04-14 22:24:34 +0600

    RSS support and all routes enabled

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\OhysBlacklistController;
 use App\Http\Controllers\Services\AnimeController;
 use App\Http\Controllers\Services\CharacterController;
 use App\Http\Controllers\Services\CompanyController;
+use App\Http\Controllers\Services\EpisodeController;
+use App\Http\Controllers\Services\OhysController;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/test', [\App\Http\Controllers\TestController::class, 'Test']);
 
 Route::prefix('minako')->group(function () {
     Route::prefix('cache')->group(function () {
@@ -36,36 +37,36 @@ Route::prefix('minako')->group(function () {
             Route::get('/search/{name}', [AnimeController::class, 'Search']);
         });
 
-//        Route::prefix('episode')->group(function () {
-//            Route::get('/{id}', [EpisodeController::class, 'GetEpisode']);
-//            Route::get('/search/{name}', [EpisodeController::class, 'Search']);
-//        });
-//
+        Route::prefix('episode')->group(function () {
+            Route::get('/{id}', [EpisodeController::class, 'GetEpisode']);
+            Route::get('/search/{name}', [EpisodeController::class, 'Search']);
+        });
+
         Route::prefix('company')->group(function () {
             Route::get('/{id}', [CompanyController::class, 'GetCompany']);
             Route::get('/search/{name}', [CompanyController::class, 'Search']);
         });
-//
+
         Route::prefix('character')->group(function () {
             Route::get('/{id}', [CharacterController::class, 'GetCharacter']);
             Route::get('/{id}/image', [CharacterController::class, 'GetImage']);
             Route::get('/search/{name}', [CharacterController::class, 'Search']);
         });
-//
-//        Route::prefix('ohys')->group(function () {
-//            Route::get('/', [OhysController::class, 'GetRecentTorrents']);
-//            Route::get('/{id}', [OhysController::class, 'GetTorrent']);
-//            Route::get('/{id}/download', [OhysController::class, 'DownloadTorrent']);
-//            Route::get('/search/{name}', [OhysController::class, 'Search']);
-//            Route::get('/service/rss', [OhysController::class, 'GetRSS']);
-//        });
-//    });
-//
-//    Route::prefix('blacklist')->group(function () {
-//        Route::get('/', [OhysBlacklistController::class, 'GetOhysBlacklistTitles']);
-//        Route::post('/', [OhysBlacklistController::class, 'CreateOhysBlacklistTitle']);
-//        Route::get('/{title_id}', [OhysBlacklistController::class, 'GetOhysBlacklistTitle']);
-//        Route::delete('/{title_id}', [OhysBlacklistController::class, 'DeleteOhysBlacklistTitle']);
-//        Route::patch('/{title_id}', [OhysBlacklistController::class, 'UpdateOhysBlacklistTitle']);
+
+        Route::prefix('ohys')->group(function () {
+            Route::get('/', [OhysController::class, 'GetRecentTorrents']);
+            Route::get('/{id}', [OhysController::class, 'GetTorrent']);
+            Route::get('/{id}/download', [OhysController::class, 'DownloadTorrent']);
+            Route::get('/search/{name}', [OhysController::class, 'Search']);
+            Route::get('/service/rss', [OhysController::class, 'GetRSS']);
+        });
+    });
+
+    Route::prefix('blacklist')->group(function () {
+        Route::get('/', [OhysBlacklistController::class, 'Get']);
+        Route::post('/', [OhysBlacklistController::class, 'Create']);
+        Route::get('/{title_id}', [OhysBlacklistController::class, 'GetFromID']);
+        Route::delete('/{title_id}', [OhysBlacklistController::class, 'Delete']);
+        Route::patch('/{title_id}', [OhysBlacklistController::class, 'Update']);
     });
 });
