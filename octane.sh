e47ef5a0575fdaae9659b1dff2b9b67c9f8bbf92
Cryental <contact@cryental.dev>
2024-04-19 15:55:49 +0600

    dummy commit

--- octane.sh
+++ octane.sh
@@ -10,7 +10,7 @@ HEX_IP="0100007F"
 
 echo "Laravel Octane Manager For Shared Hosting" >&2
 echo "Made By Cryental" >&2
-echo "Version 1.3.1" >&2
+echo "Version 1.3.2" >&2
 echo "" >&2
 # Change to the directory where your Laravel application is located
 cd "$GIT_REPO_PATH" || exit
