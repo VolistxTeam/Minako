225b50848c1f09a2b33d4fce958360eca3686bba
Shift <shift@laravelshift.com>
2024-05-05 13:11:05 +0000

    Default new `bootstrap/app.php`

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,12 +2,4 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
-    App\Providers\DTOUtilsServiceProvider::class,
-    App\Providers\HttpClientServiceProvider::class,
-    App\Providers\JikanAPIServiceProvider::class,
-    App\Providers\NyaaCrawlerServiceProvider::class,
-    App\Providers\OhysBlacklistProvider::class,
-    App\Providers\SHA256HasherServiceProvider::class,
-    App\Providers\StringOperationsServiceProvider::class,
 ];
